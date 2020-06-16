niej/envytools

commit 4897e708875ec0c982d98deab37670781f7ee0fc

cffdump: add some query-modes
Add options to only dump queried regs if at least one of them was
written since previous draw (--query-written) or if at least on of
them has a changed value since previous draw (--query-delta)

cffdump/cffdec.h
@@ -26,6 +26,21 @@
 
 #include <stdbool.h>
 
+enum {
+	/* default mode, dump all queried regs on each draw: */
+	QUERY_ALL = 0,
+
+	/* only dump if any of the queried regs were written
+	 * since last draw:
+	 */
+	QUERY_WRITTEN,
+
+	/* only dump if any of the queried regs changed since
+	 * last draw:
+	 */
+	QUERY_DELTA,
+} query_mode;
+
 struct cffdec_options {
 	unsigned gpu_id;
 	int draw_filter;
@@ -36,6 +51,8 @@ struct cffdec_options {
 	int dump_textures;
 	int decode_markers;
 	char *script;
+
+	int query_mode;     /* enum query_mode */
 	char **querystrs;
 	int nquery;
 

cffdump/cffdec.c
@@ -946,13 +946,46 @@ print_mode(int level)
 	}
 }
 
+static bool
+skip_query(void)
+{
+	switch (options->query_mode) {
+	case QUERY_ALL:
+		/* never skip: */
+		return false;
+	case QUERY_WRITTEN:
+		for (int i = 0; i < options->nquery; i++) {
+			uint32_t regbase = queryvals[i];
+			if (!reg_written(regbase)) {
+				continue;
+			}
+			if (reg_rewritten(regbase)) {
+				return false;
+			}
+		}
+		return true;
+	case QUERY_DELTA:
+		for (int i = 0; i < options->nquery; i++) {
+			uint32_t regbase = queryvals[i];
+			if (!reg_written(regbase)) {
+				continue;
+			}
+			uint32_t lastval = reg_val(regbase);
+			if (lastval != lastvals[regbase]) {
+				return false;
+			}
+		}
+		return true;
+	}
+	return true;
+}
+
 /* well, actually query and script..
  * NOTE: call this before dump_register_summary()
  */
 static void
 do_query(const char *primtype, uint32_t num_indices)
 {
-	int i;
 	int n = 0;
 
 	if ((500 <= options->gpu_id) && (options->gpu_id < 700)) {
@@ -965,7 +998,13 @@ do_query(const char *primtype, uint32_t num_indices)
 		bin_y2 = scissor_br >> 16;
 	}
 
-	for (i = 0; i < options->nquery; i++) {
+	if (script_draw)
+		script_draw(primtype, num_indices);
+
+	if (skip_query())
+		return;
+
+	for (int i = 0; i < options->nquery; i++) {
 		uint32_t regbase = queryvals[i];
 		if (reg_written(regbase)) {
 			uint32_t lastval = reg_val(regbase);
@@ -991,9 +1030,6 @@ do_query(const char *primtype, uint32_t num_indices)
 
 	if (n > 1)
 		printf("\n");
-
-	if (script_draw)
-		script_draw(primtype, num_indices);
 }
 
 static void

cffdump/cffdump.c
@@ -87,6 +87,12 @@ static void print_usage(const char *name)
 			"\t                   each draw; multiple --query/-q args can be given to\n"
 			"\t                   dump multiple registers; register can be specified\n"
 			"\t                   either by name or numeric offset\n"
+			"\t--query-all      - in query mode, show all queried regs on each draw\n"
+			"\t                   (default query mode)\n"
+			"\t--query-written  - in query mode, show queried regs on draws if any of\n"
+			"\t                   them have been written since previous draw\n"
+			"\t--query-delta    - in query mode, show queried regs on draws if any of\n"
+			"\t                   them have changed since previous draw\n"
 			"\t-h, --help       - show this message\n"
 			, name);
 	exit(2);
@@ -101,6 +107,9 @@ static const struct option opts[] = {
 	{ "pager",           no_argument, &interactive,           1 },
 	{ "textures",        no_argument, &options.dump_textures, 1 },
 	{ "show-compositor", no_argument, &show_comp,             1 },
+	{ "query-all",       no_argument, &options.query_mode,    QUERY_ALL },
+	{ "query-written",   no_argument, &options.query_mode,    QUERY_WRITTEN },
+	{ "query-delta",     no_argument, &options.query_mode,    QUERY_DELTA },
 
 	/* Long opts with short alias: */
 	{ "verbose",   no_argument,       0, 'v' },
@@ -185,6 +194,11 @@ int main(int argc, char **argv)
 
 	if (ret)
 		print_usage(argv[0]);
+
+	if (options.query_mode && !options.nquery) {
+		fprintf(stderr, "query options only valid in query mode!\n");
+		print_usage(argv[0]);
+	}
 
 	script_finish();
 

commit 999f3f972fb24d5bf7f36c8c949ed3cd6c2862de

fix build error
Fixes: 4897e708 ("cffdump: add some query-modes")

cffdump/cffdec.h
@@ -26,7 +26,7 @@
 
 #include <stdbool.h>
 
-enum {
+enum query_mode {
 	/* default mode, dump all queried regs on each draw: */
 	QUERY_ALL = 0,
 
@@ -39,7 +39,7 @@ enum {
 	 * last draw:
 	 */
 	QUERY_DELTA,
-} query_mode;
+};
 
 struct cffdec_options {
 	unsigned gpu_id;